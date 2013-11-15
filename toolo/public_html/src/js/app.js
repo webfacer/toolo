f76facbfa1dee3745b7f4953dc752044a3bd865a
Ivan Ilic <ivan.ilic@gnius.at>
2013-11-15 02:43:47 +0100

    few changes
    added using bower packages
    added directive with attribute value (top/right/bottom/left)

--- toolo/public_html/src/js/app.js
+++ toolo/public_html/src/js/app.js
@@ -5,19 +5,21 @@ var toolo = angular.module('toolo', [])
 .directive('wfToolo',['$compile', '$log', function($compile, $log){
     return {
         restrict : 'A',
-		link : function(scope, element, attrs, parentFunction){
-		    if(attrs.title) {
+		link : function(scope, element, attr, parentFunction){
+		    if(attr.title) {
 		        var tooltipArea = angular.element(element[0]);
 
 		        element
 		        .on('mouseover', function() {
-		            var text = attrs.title;
-		            var el = $compile('<span class="tip right">' + text + '</span>')(scope);
+		            var text = attr.title;
+		            var el = $compile('<span class="tip ' + ( attr.wfToolo ? attr.wfToolo : 'top' ) + '">' + text + '</span>')(scope);
 		            tooltipArea.append(el);
 		        })
                 .on('mouseout', function() {
                     tooltipArea[0].lastChild.remove();
                 });
+                
+                element.removeAttr('title');
             }
             else {
                 $log.info(' Info (No \'title\' attribute is set):  You might forgot to add a `title` text! In order to use toolo you must use e.g. `title="Hello World!"` in the same directive you want a tooltip to appear.');
